1dd64695e6e60323f1ee807a2e552370b1d73f80
malikgaurav626 <malikgaurav626@gmail.com>
2023-10-06 23:47:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ["three"],
+    rollupOptions: {
+      external: ["three"],
+    },
   },
 });
