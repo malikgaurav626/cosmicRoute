d16597a4d15be2ac31eac6dc90bc34d9194c3421
malikgaurav626 <malikgaurav626@gmail.com>
2023-10-06 23:40:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
+  },
+  resolve: {
+    alias: {
+      three: "three",
+    },
   },
 });
