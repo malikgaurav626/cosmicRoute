9fd3c113f5e8ff3d09e17a7eefd920a9cda164a4
malikgaurav626 <malikgaurav626@gmail.com>
2023-10-07 08:50:53 +0530

    add threejs as dependency
    added threejs as dependency using css

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["three/examples/jsm/controls/orbitcontrols"],
-  },
   build: {
     rollupOptions: {
-      external: "^three/examples/jsm/controls/[^/]+$",
+      external: ["three"],
     },
   },
 });
